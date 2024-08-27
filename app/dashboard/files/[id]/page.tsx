93ab64aabec28616c3617b9d61d5c901f70807dd
Adam_Mikulas <miku.adam@gmail.com>
2024-08-27 13:03:44 +0200

    feat(): add react pdf viewer

--- app/dashboard/files/[id]/page.tsx
+++ app/dashboard/files/[id]/page.tsx
@@ -1,11 +1,38 @@
+import PdfView from "@/components/PdfView";
+import { adminDb } from "@/firebaseAdmin";
+import { auth } from "@clerk/nextjs/server";
+
 type Props = {
   params: {
     id: string;
   };
 };
 
-function ChatToFilePage({ params: { id } }: Props) {
-  return <div>ChatToFilePage {id}</div>;
+async function ChatToFilePage({ params: { id } }: Props) {
+  auth().protect();
+  const { userId } = await auth();
+
+  if (!userId) {
+    return null;
+  }
+
+  const ref = await adminDb
+    .collection("users")
+    .doc(userId)
+    .collection("files")
+    .doc(id)
+    .get();
+
+  const url = ref.data()?.downloadURL;
+  console.log(url);
+  return (
+    <div className="grid lg:grid-cols-5 h-full overflow-hidden">
+      <div className="col-span-5 lg:col-span-2 overflow-y-auto">Chat</div>
+      <div className="col-span-5 lg:col-span-3 bg-gray-100 border-r-2 lg:border-indigo-600 lg:-order-1 overflow-auto">
+        <PdfView url={url} />
+      </div>
+    </div>
+  );
 }
 
 export default ChatToFilePage;
