186b1626cfea51e94b41f4ce8222294ce835489d
Adam_Mikulas <miku.adam@gmail.com>
2024-08-27 17:15:01 +0200

    feat(): add my documents

--- components/Documents.tsx
+++ components/Documents.tsx
@@ -1,8 +1,36 @@
+import { adminDb } from "@/firebaseAdmin";
+import { auth } from "@clerk/nextjs/server";
+import Document from "./Document";
 import PlaceholderDocument from "./PlaceholderDocument";
 
-function Documents() {
+async function Documents() {
+  auth().protect();
+  const { userId } = await auth();
+
+  if (!userId) {
+    throw new Error("User not found");
+  }
+
+  const documentSnapshot = await adminDb
+    .collection("users")
+    .doc(userId)
+    .collection("files")
+    .get();
+
   return (
     <div className="flex flex-wrap p-5 bg-gray-100 justify-center lg:justify-start rounded-sm gap-5 max-w-7xl mx-auto">
+      {documentSnapshot.docs.map((doc) => {
+        const { name, downloadURL, size } = doc.data();
+        return (
+          <Document
+            key={doc.id}
+            id={doc.id}
+            downloadURL={downloadURL}
+            size={size}
+            name={name}
+          />
+        );
+      })}
       <PlaceholderDocument />
     </div>
   );
