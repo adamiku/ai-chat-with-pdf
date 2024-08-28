d320202fab8a20bb2bc8f858b75aced427f462d8
Adam_Mikulas <miku.adam@gmail.com>
2024-08-28 08:40:52 +0200

    feat(): add upgradebutton

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { Toaster } from "@/components/ui/toaster";
 import { ClerkProvider } from "@clerk/nextjs";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
@@ -21,6 +22,7 @@ export default function RootLayout({
         <body
           className={`${inter.className} min-h-screen h-screen overflow-hidden flex flex-col`}
         >
+          <Toaster />
           {children}
         </body>
       </html>
